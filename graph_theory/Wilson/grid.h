0dcc89b899e2733de5878f4e5b2a4e81c203f151
Eli <eli@pluto.local>
2024-03-01 10:29:46 -0500

    setting up some pseudocode for wilsons algorithm, changed GridCell struct to contain two enums to simplify things (i think)

--- graph_theory/Wilson/grid.h
+++ graph_theory/Wilson/grid.h
@@ -1,12 +1,22 @@
-// typedef struct Wall {
-//     Vector2 start_point;
-//     Vector2 end_point;
-// } Wall;
 
+
+typedef enum {
+    NOT_PART_OF_MAZE,
+    PART_OF_MAZE_WALK,
+    PART_OF_MAZE_FINAL,
+} CellState;
+
+typedef enum {
+    NORTH,
+    EAST,
+    SOUTH,
+    WEST,
+    NONE
+} ExitDirection;
 
 typedef struct GridCell {
     Vector2 top_left;
-    bool part_of_maze;
-    bool part_of_final_maze;
+    CellState cell_state;      // bool part_of_maze; // bool part_of_final_maze;
     struct GridCell *next_node;
+    ExitDirection cell_exit_direction;
 } GridCell;
--- graph_theory/Wilson/wilson.c
+++ graph_theory/Wilson/wilson.c
@@ -8,7 +8,7 @@
 
 // Function declarations
 int genRandNum(int min, int max);
-int **performRandomWalk(GridCell **grid, int r, int c, int random_start_row, int random_start_col);
+void performRandomWalk(GridCell **grid, int r, int c, int random_start_row, int random_start_col);
 
 GridCell **generateWilsonsMaze(GridCell **grid, int r, int c, int node_distance) {
 
@@ -16,6 +16,12 @@ GridCell **generateWilsonsMaze(GridCell **grid, int r, int c, int node_distance)
     int random_start_row, random_start_col; 
     int num_of_cells_in_maze = 0;
     GridCell *start_cell;
+    GridCell *curr_cell;
+    // GridCell *cells_to_add_to_final_maze;
+    GridCell *cell_walk;
+    GridCell *cell_walk_next;
+
+    bool cell_not_in_maze_found;
     
 
     // 1. Choose any vertex at random and add it to the UST.
@@ -24,20 +30,50 @@ GridCell **generateWilsonsMaze(GridCell **grid, int r, int c, int node_distance)
     printf("%d, %d\n", random_start_row, random_start_col);
     start_cell = &grid[random_start_row][random_start_col];
 
-    start_cell->part_of_final_maze = true;
+    start_cell->cell_state = PART_OF_MAZE_FINAL;
     num_of_cells_in_maze++;
 
 
-     // LOOP UNTIL all vertices have been added to the UST (i.e. All vertices are part of maze)
+     // LOOP UNTIL all vertices have been added to the UST (i.e. All vertices are part of final maze)
     while (num_of_cells_in_maze < r*c) {
 
             // 2. Select any vertex that is not already in the UST and perform a random walk until you encounter a vertex that is in the UST.
-            random_start_row = genRandNum(0, r-1);
-            random_start_col = genRandNum(0, c-1);
-            performRandomWalk(grid, r, c, random_start_row, random_start_col);
+            cell_not_in_maze_found = false;
+            while (!cell_not_in_maze_found) {
+                random_start_row = genRandNum(0, r-1);
+                random_start_col = genRandNum(0, c-1);
+                if (grid[random_start_row][random_start_col].part_of_final_maze) {
+                    cell_not_in_maze_found = true;
+                }
+            }
+            // 
+            // 
+            // 
+            // 
+            // MAYBE USE AN ENUM TO KEEP TRACK OF WHICH DIRECTION TO EXIT EACH CELL (ENUM DIRECTION {LEFT, RIGHT, UP DOWN})
+            // COULD ALSO USE AN ENUM TO KEEP TRACK OF WHETHER IT IS PART OF A MAZE-WALK OR PART OF THE MAZE-FINAL
+            // 
+            // 
+            // 
+            // 
+           performRandomWalk(grid, r, c, random_start_row, random_start_col);
+           cell_walk = &grid[random_start_row][random_start_col];
+           cell_walk_next = cell_walk->next_node;
 
+            while (cell_walk_next != NULL) {
+                
+            }
+
+            // NEED TO FIGURE OUT WAY TO INDICATE WHICH SIDE OF CELL THAT THE MAZE CONTINUES ON
             // 3. Add the vertices and edges touched in the random walk to the UST.
-        // END LOOP
+            // for (int i = 0; i < r; i++) {
+            //     if (cells_to_add_to_final_maze[i][0] == -1) break; 
+
+            //     curr_cell = &grid[cells_to_add_to_final_maze[i][0]][cells_to_add_to_final_maze[i][1]];
+            //     curr_cell->part_of_final_maze = true;
+            // }
+
+    // END LOOP
     }
 
     return grid;
@@ -45,20 +81,46 @@ GridCell **generateWilsonsMaze(GridCell **grid, int r, int c, int node_distance)
 
 
 // Helper function to perform a random walk starting at a random row,col
-int **performRandomWalk(GridCell **grid, int r, int c, int random_start_row, int random_start_col) {
+void performRandomWalk(GridCell **grid, int r, int c, int random_start_row, int random_start_col) {
 
-    int **cells_added_maze;
-    cells_added_maze = malloc(r * sizeof(int *));
+    // GridCell *cells_added_maze;
+    // cells_added_maze = malloc(r * sizeof(int *));
     // for (int i = 0; i < r; i++) cells_added_maze[i] = malloc(2 * sizeof(int))
 
-    GridCell *start_cell;
+    GridCell *start_cell, *curr_cell;
     start_cell = &grid[random_start_row][random_start_col];
     
     bool loop_found = false;
+    int move_direction;
     while (!loop_found) {
-        // choose one of 4 cardinal directions to move in
+
+        // choose one of the 4 cardinal directions to move in
+        //      N
+        //   W     E
+        //      S
+
+        move_direction = genRandNum(1, 4);
+        if (move_direction == 1) {
+            // move North (r-1)
+            // check if r-1 is valid
+            
+        }
+        if (move_direction == 2) {
+            // move East (c+1)
+            // check if c+1 is valid
+        }
+        if (move_direction == 3) {
+            // move South (r+1)
+            // check if r+1 is valid
+        }
+        if (move_direction == 4) {
+            // move West (c-1)
+            // check if c-1 is valid
+        }
 
     }
+
+    return;
 }
 
 
