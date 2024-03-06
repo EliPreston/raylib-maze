73d2e3354384db989128882625942d8215297741
Eli <eli@pluto.local>
2024-03-05 19:13:46 -0500

    added a little gif to show the cellular automation

--- README.md
+++ README.md
@@ -10,7 +10,7 @@
 ##### Wilson's Algorithm 
 - Still a WIP, but working for the most part
 - Very unoptomized, code needs to be refactored, cleaned, and optimized where possible (I'm not super concerned with crazy optimization)
-![Alt Screenshot of a maze created](/img/Wilson_semi_working_v1.png)
+![Alt Screenshot of a maze created](/img/graph_theory/Wilson_semi_working_v1.png)
 
 ##### Prim's Algorithm
 - To Be Completed
@@ -21,10 +21,13 @@
 ##### Interesting patterns
 1. Mazectric rulestring (B3/S1234 same as 1234/3)
 
-![Alt Screenshot of initial population generated](/img/Initial_population.png)
+![Alt gif of cells](/img/cell_automation/cell_demo.gif)
+- It looks so chopy because at this point in time I had it so that pressing 's' updated the population by one generation each time (so I was repeatedly button mashing 's')
 
-![Alt Screenshot of grid expanding](/img/Population_expanding.png)
+![Alt Screenshot of initial population generated](/img/cell_automation/Initial_population.png)
 
-![Alt Screenshot of grid hitting edge of window](/img/Population_reaching_edge.png)
+![Alt Screenshot of grid expanding](/img/cell_automation/Population_expanding.png)
 
-![Alt Screenshot of final resulting grid](/img/Population_fill.png)
+![Alt Screenshot of grid hitting edge of window](/img/cell_automation/Population_reaching_edge.png)
+
+![Alt Screenshot of final resulting grid](/img/cell_automation/Population_fill.png)
--- cellular_automota/V2/main.c
+++ cellular_automota/V2/main.c
@@ -1,6 +1,7 @@
     // Header files
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // External libraries
 #include "raylib.h"
@@ -19,6 +20,15 @@ int main(int argc, char *argv[]) {
     const int width = 1000;
     const int height = 850;
     const int node_distance = 5;
+
+    int generation_count = 1;
+    char gen_count_txt[] = "Generation #: ";
+    char gen_count_int[20];
+    snprintf(gen_count_int, sizeof(gen_count_int), "%d", generation_count);
+
+    char *generation_num = malloc(strlen(gen_count_txt) + 20 + 1); // +1 for the null-terminator
+    strcpy(generation_num, gen_count_txt);
+    strcat(generation_num, gen_count_int);
 
      // Set up grid
     int num_rows = (height / node_distance);
@@ -40,11 +50,24 @@ int main(int argc, char *argv[]) {
             for (int idx = 0; idx < num_rows; idx++) free(cells_grid[idx]);
             free(cells_grid);
             cells_grid = createCellGrid(width, height, node_distance);
-            
+
+            free(generation_num);
+            generation_count = 1;
+            snprintf(gen_count_int, sizeof(gen_count_int), "%d", generation_count);
+            generation_num = malloc(strlen(gen_count_txt) + 20 + 1); // +1 for the null-terminator
+            strcpy(generation_num, gen_count_txt);
+            strcat(generation_num, gen_count_int);  
         }
 
         if (IsKeyPressed(KEY_S)) {
             cells_grid = cellAutomationUpdateGrid(cells_grid, num_rows, num_cols);
+
+            free(generation_num);
+            generation_count++;
+            snprintf(gen_count_int, sizeof(gen_count_int), "%d", generation_count);
+            generation_num = malloc(strlen(gen_count_txt) + 20 + 1); // +1 for the null-terminator
+            strcpy(generation_num, gen_count_txt);
+            strcat(generation_num, gen_count_int);
         }
         
 
@@ -55,6 +78,11 @@ int main(int argc, char *argv[]) {
             
             // Draw grid
             drawCellGrid(cells_grid, num_rows, num_cols);
+
+            DrawRectangle(10, 10, 100, 40, WHITE);
+            DrawFPS(20, 20);
+            DrawText(generation_num, 20, 40, 10, RED);
+            // DrawTextEx(Font font, const char *text, Vector2 position, float fontSize, float spacing, Color tint);
             
 
         EndDrawing();
